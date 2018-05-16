22c7401faf483d55aa8ec29f980f7ea636c41636
Sergei Zaliubovskii <sergey.zalubovskiy@MacBook-Pro-Sergei.local>
2018-05-16 14:23:17 +0200

    refactoring

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import ThemeProvider from 'styled-components'
 
 import NavigationPanel from './panel'
 import NavigationContent from './content'
